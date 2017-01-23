a67e349b241736fdc5fd69f8a44e317119e6b4c7
Humakt83 <urlact@yahoo.com>
2017-01-23 20:26:46 +0200

    Illuminates the ocean below

--- poem.js
+++ poem.js
@@ -1,6 +1,7 @@
 var poemArray = [
     { poem: 'A vast ocean of darkness', transition: function() {} },
-    { poem: 'A shining ligth', transition: shiningLight }
+    { poem: 'A shining ligth', transition: shiningLight },
+    { poem: 'Illuminates the ocean below', transition: illuminateOcean}
 ];
 
 initialize();
@@ -23,4 +24,24 @@ function changePoem(poem) {
 
 function shiningLight() {
     d3.select('#background').attr('class', 'shiningLight');    
+}
+
+function illuminateOcean() {
+    var waves = d3.select('#background')
+    .append('div')
+    .attr('class', 'illuminateOcean')
+    .append('div')
+    .attr('class', 'waves');
+    for (var i = 0; i < 20; i++) {
+        var positionLeft = i === 0 ? '0%': ((i / 20) * 100) + '%';
+        waves.append('div')
+        .attr('class', 'waveElement')
+        .style('left', positionLeft);
+        if (i > 0) {
+            var positionLeftNegative = ((i / 20) * -100) + '%';
+            waves.append('div')
+            .attr('class', 'waveElement')
+            .style('left', positionLeftNegative);
+        }
+    }
 }
